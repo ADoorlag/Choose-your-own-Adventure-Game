1f9275e0e278e241996f16e3af9d8a5b9435f6c2
ADoorlag <Alex.Doorlag@gmail.com>
2024-11-13 21:40:49 -0500

    A simple choose your own adventure game built using nested if statements in Python.

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -2,7 +2,7 @@ name = input("What is your name? ")
 
 print("Welcome to the Choose Your Own Adventure Game " + name + "!")
 print("You are a hero in a world full of danger and adventure.")
-print("Your goal is to find the treasure and escape the dungeon.")
+print("Your goal is to find the treasure and escape the forest.")
 print("You start on a dirt road that has ended at a river. You have three choices to make:")
 print("1. Go left")
 print("2. Go right")
@@ -31,7 +31,22 @@ if choice == "1":
         
 elif choice == "2":
     print("You have chosen to go right.")
-    print("You have been attacked by a monster and died.")
+    print("You have been attacked by a monster and were injured.")
+    print("You have three choices to make:")
+    print("1. Fight the monster")
+    print("2. Run away and try to treat your wounds")
+    print("3. Use a healing potion")
+    choice = input("Enter 1, 2, or 3: ")
+    if choice == "1":
+        print("You have chosen to fight the monster.")
+        print("You have been killed by the monster.")
+    elif choice == "2":
+        print("You have chosen to run away and try to treat your wounds.")
+        print("You successfully escaped the monster, but your wounds became infected and you died")
+    elif choice == "3":
+        print("You have chosen to use a healing potion.")
+        print("You have been healed and restored to full strength.")
+        
 elif choice == "3":
     print("You have chosen to swim across the river.")
     print("You have been drowned in the river and died.")
